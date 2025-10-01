226f7bfd975c52ce78b12442a45f21dae0976f4f
Karen Behlarian <Gladiatorkaren@gmail.com>
2025-10-01 19:29:26 +0300

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -3,7 +3,7 @@ import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => (
   <article
-    className={`TodoInfo--completed ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
     {todo.user && <UserInfo user={todo.user} />}
